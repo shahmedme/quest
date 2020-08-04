314ab843ac101395858d1a55f4e98de0dbf4400b
shakilahmmeed <shakilahmmeed@gmail.com>
2020-08-04 23:57:19 +0600

    style: homepage layout

--- main.go
+++ main.go
@@ -9,14 +9,14 @@ import (
 
 func main() {
 	r := mux.NewRouter()
-
 	r.HandleFunc("/", home)
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 
 	http.ListenAndServe(":7000", r)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	tmpl := template.Must(template.ParseFiles("templates/index.html", "templates/components/navbar.html", "templates/components/sidebar.html", "templates/partials/meta.html"))
 	tmpl.Execute(w, "context")
 	return
 }
